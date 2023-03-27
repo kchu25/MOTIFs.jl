df71d3f0486ce7c9f0d6b2f44f3720753087fc76
Documenter.jl <documenter@juliadocs.github.io>
2023-03-27 18:30:32 +0000

    build based on 14de7f8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MOTIFs","category":"page"},{"location":"#MOTIFs","page":"Home","title":"MOTIFs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MOTIFs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MOTIFs]","category":"page"}]
+}
